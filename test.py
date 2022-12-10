c6b864219cb49e8a455421d70773155910003291
project99 <mike.s.qiu@gmail.com>
2022-12-10 14:32:57 +1100

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+class menu:
+  def __init__(self, food, price):
+    self.item = food
+    self.pay = price
+
+  def takeaway(self):
+    print(self.item, self.pay)
+
+class sub_menu(menu):
+  pass
+
+x = sub_menu("beef", 10)
+
+x.takeaway()
+  pass
