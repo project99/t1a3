2d3ac829df2c2c7b2c759aec9b41e635145fd53d
project99 <mike.s.qiu@gmail.com>
2022-12-10 17:03:36 +1100

    med list extend trial-incomplete

--- data.py
+++ data.py
@@ -2,7 +2,13 @@ def pt_list ():
   pt1 = {"name": "mary", "meds": ["ramipril", "asprin"]}
   pt2 = {"name": "ned", "meds": ["asprin", "plavix", "symbicort"]}
   pt3 = {"name": "jack", "meds": ["diabex", "novorapid", "statin"]}
-  pt1["meds"].extend(["statin"])
-  print (pt1)
+  pt1["meds"].extend(["statin", "movicol"])
+  print (pt2)
+
+def add_meds (current_pts, name):
+  for searched_pt in current_pt:
+    if searched_pt ["name"] == ned:
+      
+
 
 pt_list ()
