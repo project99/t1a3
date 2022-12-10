7e4b87d03d89564a5e5237e3ea14af3b2b548eb3
project99 <mike.s.qiu@gmail.com>
2022-12-10 16:47:19 +1100

    extend list  trial

--- data.py
+++ data.py
@@ -1,5 +1,8 @@
 def pt_list ():
   pt1 = {"name": "mary", "meds": ["ramipril", "asprin"]}
+  pt2 = {"name": "ned", "meds": ["asprin", "plavix", "symbicort"]}
+  pt3 = {"name": "jack", "meds": ["diabex", "novorapid", "statin"]}
+  pt1["meds"].extend(["statin"])
   print (pt1)
 
 pt_list ()
